b20b7caac3a1d1ba0ffa1d54552134212fc3d022
takusemba <takusemba.ele@gmail.com>
2019-11-12 21:22:39 +0900

    fix import

--- app/src/test/java/com/example/sampleapp/UserViewModelTest.kt
+++ app/src/test/java/com/example/sampleapp/UserViewModelTest.kt
@@ -15,7 +15,10 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
+@RunWith(JUnit4::class)
 class UserViewModelTest {
 
     @get:Rule
